b8f7e3328ed6359e7278cd1813eee466da0e5097
Jawad Ali <alijamil734@gmail.com>
2021-11-25 19:56:39 +0500

    made testimonial base component

---
+++ src/components/Base/BaseTestimonial.vue
@@ -0,0 +1,14 @@
+<script lang="ts" setup>
+</script>
+
+
+<template>
+  <div class="flex justify-around w-4/5 mx-auto px-10">
+    <img class="w-96 h-96 rounded-full object-cover" src="https://images.unsplash.com/photo-1593642634367-d91a135587b5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxzZWFyY2h8MXx8bGFwdG9wfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60" alt="">
+    <div class="max-w-xl flex flex-col justify-center">
+      <h1 class="text-primary font-semibold text-2xl">Jawad Ali</h1>
+      <p class="text-secondary my-2 font-semibold text-md">Co-Founder</p>
+      <p class="text-gray-500 text-xs max-w-sm tracking-wider text-justify my-5">"Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur dolores a suscipit? Voluptates, eaque saepe hic quam assumenda cupiditate, sint veritatis corporis repudiandae, nesciunt id enim voluptas. Repellat, dolorem libero"</p>
+    </div>
+  </div>
+</template>
