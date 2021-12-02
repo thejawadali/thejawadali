3d894a0e7fc7259d55e815196828a18d15b56e6a
Jawad Ali <alijamil734@gmail.com>
2021-12-02 20:42:28 +0500

    🐛 FIX: in md screens text aligned left headings of testimonials

--- src/components/Base/BaseTestimonial.vue
+++ src/components/Base/BaseTestimonial.vue
@@ -12,10 +12,10 @@ const props = defineProps({
   <div class="flex flex-col md:flex-row justify-between w-full mx-auto bg-white">
     <img class="w-full h-64 md:w-72 md:h-72 lg:w-80 lg:h-80 xl:w-96 xl:h-96 md:rounded-full object-cover" :src="img" />
     <div class="w-auto flex-grow p-4 md:p-0 flex flex-col justify-center md:ml-5 xl:ml-20">
-      <h1 class="text-primary font-semibold text-2xl">
+      <h1 class="text-primary font-semibold text-2xl md:text-left">
         {{ customerName }}
       </h1>
-      <p class="text-secondary md:my-0 font-semibold text-md">{{ desg }}</p>
+      <p class="text-secondary md:my-0 md:text-left font-semibold text-md">{{ desg }}</p>
       <p
         class="text-gray-400 text-xs lg:text-sm w-full tracking-wider text-justify mt-2 md:mt-4"
       >
