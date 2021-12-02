f2f0c44aeac2672bcfc0ef366f62ba8044cd973c
Jawad Ali <alijamil734@gmail.com>
2021-12-02 19:55:30 +0500

    made carousel for testimonials

--- src/components/Base/BaseTestimonial.vue
+++ src/components/Base/BaseTestimonial.vue
@@ -1,26 +1,25 @@
 <script lang="ts" setup>
-const feed = {
-  img: "https://images.unsplash.com/photo-1452860606245-08befc0ff44b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8cHJvamVjdHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60",
-  customerName: "Jawad Ali",
-  desg: "Co-Founder",
-  details:
-    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur dolores a suscipit? Voluptates, eaque saepe hic quam assumenda cupiditate, sint veritatis corporis repudiandae, nesciunt id enim voluptas. Repellat, dolorem libero",
-};
+const props = defineProps({
+  img: String,
+  customerName: String,
+  desg: String,
+  details: String
+})
 </script>
 
 
 <template>
   <div class="flex flex-col md:flex-row justify-between w-full mx-auto bg-white">
-    <img class="w-full h-64 md:w-72 md:h-72 lg:w-80 lg:h-80 xl:w-96 xl:h-96 md:rounded-full object-cover" :src="feed.img" />
+    <img class="w-full h-64 md:w-72 md:h-72 lg:w-80 lg:h-80 xl:w-96 xl:h-96 md:rounded-full object-cover" :src="img" />
     <div class="w-auto flex-grow p-4 md:p-0 flex flex-col justify-center md:ml-5 xl:ml-20">
       <h1 class="text-primary font-semibold text-2xl">
-        {{ feed.customerName }}
+        {{ customerName }}
       </h1>
-      <p class="text-secondary md:my-0 font-semibold text-md">{{ feed.desg }}</p>
+      <p class="text-secondary md:my-0 font-semibold text-md">{{ desg }}</p>
       <p
         class="text-gray-400 text-xs lg:text-sm w-full tracking-wider text-justify mt-2 md:mt-4"
       >
-        "{{ feed.details }}"
+        "{{ details }}"
       </p>
     </div>
   </div>
--- src/components/Testimonials.vue
+++ src/components/Testimonials.vue
@@ -1,5 +1,34 @@
 <script lang="ts" setup>
-import Testimonial from "./Base/BaseTestimonial.vue";
+import BaseTestimonial from "./Base/BaseTestimonial.vue";
+import "vue3-carousel/dist/carousel.css";
+import { Carousel, Slide, Pagination} from "vue3-carousel";
+
+const feeds = [
+  {
+    id: 1,
+    img: "https://images.unsplash.com/photo-1452860606245-08befc0ff44b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8cHJvamVjdHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60",
+    customerName: "Jawad Ali",
+    desg: "Co-Founder",
+    details:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur dolores a suscipit? Voluptates, eaque saepe hic quam assumenda cupiditate, sint veritatis corporis repudiandae, nesciunt id enim voluptas. Repellat, dolorem libero",
+  },
+  {
+    id: 2,
+    img: "http://placeimg.com/640/480/cats",
+    customerName: "Some random pic from vs code",
+    desg: "Co-Founder",
+    details:
+      "Sapiente nulla iure. Eum et rerum rem ut enim voluptatibus. Est doloribus fugit. Quis et id id esse assumenda at possimus tempora. Omnis dolorem odio iure ipsum asperiores quae dolorum facere. Quaerat est eos qui itaque.",
+  },
+  {
+    id: 3,
+    img: "http://placeimg.com/640/480/cats",
+    customerName: "Another slide",
+    desg: "Co-Founder",
+    details:
+      "Sapiente nulla iure. Eum et rerum rem ut enim voluptatibus. Est doloribus fugit. Quis et id id esse assumenda at possimus tempora. Omnis dolorem odio iure ipsum asperiores quae dolorum facere. Quaerat est eos qui itaque.",
+  },
+];
 </script>
 
 
@@ -8,8 +37,26 @@ import Testimonial from "./Base/BaseTestimonial.vue";
     <p class="text-primary px-8 lg:px-0 text-5xl lg:text-7xl font-extralight">
       <strong>They Trusted</strong> on my skills
     </p>
-    <div class="my-10 w-4/5 mx-auto md:max-w-5xl">
-      <Testimonial />
+    <div class="my-10 w-4/5 mx-auto md:max-w-5xl border-2 rounded md:p-10">
+      <Carousel
+        :autoplay="2500"
+        :items-to-show="1"
+        wrapAround
+        pause-autoplay-on-hover
+      >
+        <Slide v-for="feed in feeds" :key="feed.id">
+          <BaseTestimonial
+            :img="feed.img"
+            :desg="feed.desg"
+            :details="feed.details"
+            :customer-name="feed.customerName"
+          />
+        </Slide>
+
+        <template #addons>
+          <Pagination />
+        </template>
+      </Carousel>
     </div>
   </div>
 </template>
