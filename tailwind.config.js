6999329f5849646146c398afab01a9a8bd1d0a6e
Jawad Ali <alijamil734@gmail.com>
2022-01-04 11:54:44 +0500

    update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
 module.exports = {
-  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
+  purge: [
+    "./src/**/*.{html,js,css}"
+  ],
+  darkMode: 'media',
   theme: {
     extend: {
       colors: {
